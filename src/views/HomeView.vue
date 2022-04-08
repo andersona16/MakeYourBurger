e07cd806b819ea25279dc4ab59c34b931251fc64
Anderson <andersonaraujoc1@gmail.com>
2022-04-07 21:54:39 -0300

    Formulário
    Finalizando o formulário

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,25 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>Home</h1>
+    <h1>Monte o seu burguer:</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue";
+import BurgerForm from "../components/BurgerForm.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Banner,
+    BurgerForm,
+  },
+};
 </script>
+
+
+
+<style scoped>
+</style>
